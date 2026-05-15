1e6d33891949cebdafd704bc9abef4db787ad281
Fábio Silva <fabio.silva12022001@gmail.com>
2026-05-14 21:24:48 -0300

    docs: delete datahora.js

--- datahora.js
+++
@@ -1,7 +0,0 @@
-
-data = new Date
-
-diaSemana = new Array('Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb')
-mes = new Array('Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez')
-
-document.write(diaSemana[data.getDay()] + ', ' + data.getDate() + ' de ' + mes[data.getMonth()] + ' de ' + data.getFullYear())
